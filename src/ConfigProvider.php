b9377a021bcc7d218167673796b482f145d7e862
az <asdfdsa@fdsafsda.com>
2024-07-30 14:23:59 +0700

    init

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-
 namespace Mine;
 
 class ConfigProvider
@@ -10,28 +9,35 @@ class ConfigProvider
     public function __invoke(): array
     {
         return [
-            // 合并到  config/autoload/dependencies.php 文件
+            // 合并到 config/autoload/dependencies.php 文件
             'dependencies' => [],
-            // 合并到  config/autoload/annotations.php 文件
-            'annotations' => [
+            // 合并到 config/autoload/annotations.php 文件
+            'annotations'  => [
                 'scan' => [
                     'paths' => [
                         __DIR__,
+                        __DIR__.'/../src', // 添加 JWT 插件的路径
                     ],
                 ],
             ],
             // 默认 Command 的定义，合并到 Hyperf\Contract\ConfigInterface 内，换个方式理解也就是与 config/autoload/commands.php 对应
-            'commands' => [],
+            'commands'     => [],
             // 与 commands 类似
-            'listeners' => [],
+            'listeners'    => [],
             // 组件默认配置文件，即执行命令后会把 source 的对应的文件复制为 destination 对应的的文件
-            'publish' => [
+            'publish'      => [
                 [
-                    'id' => 'config',
-                    'description' => 'mineadmin config file.', // 描述
+                    'id'          => 'config',
+                    'description' => 'mineadmin config file.',                   // 描述
                     // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
-                    'source' => __DIR__ . '/../publish/mineadmin.php',  // 对应的配置文件路径
-                    'destination' => BASE_PATH . '/config/autoload/mineadmin.php', // 复制为这个路径下的该文件
+                    'source'      => __DIR__.'/../publish/mineadmin.php',        // 对应的配置文件路径
+                    'destination' => BASE_PATH.'/config/autoload/mineadmin.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id'          => 'jwt',
+                    'description' => 'jwt-auth',
+                    'source'      => __DIR__.'/../publish/jwt.php',
+                    'destination' => BASE_PATH.'/config/autoload/jwt.php',
                 ],
             ],
             // 亦可继续定义其它配置，最终都会合并到与 ConfigInterface 对应的配置储存器中
