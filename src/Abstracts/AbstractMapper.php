dda6055c874358a795710e10626e9a44dd3679b5
az <asdfdsa@fdsafsda.com>
2024-08-02 11:06:33 +0700

    init

--- src/Abstracts/AbstractMapper.php
+++ src/Abstracts/AbstractMapper.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace Mine\Abstracts;
 
 use Hyperf\Context\Context;
+use Mine\MillisModel;
 use Mine\MineModel;
 use Mine\Traits\MapperTrait;
 
@@ -20,7 +21,7 @@ abstract class AbstractMapper
     use MapperTrait;
 
     /**
-     * @var class-string|MineModel
+     * @var class-string|MineModel|MillisModel
      */
     public $model;
 
@@ -38,12 +39,30 @@ abstract class AbstractMapper
         return $this->getAttributes()[$name] ?? '';
     }
 
+    /**
+     * 魔术方法，设置类属性.
+     * @param  string  $name
+     * @param  mixed  $value
+     */
+    public function __set(string $name, mixed $value): void
+    {
+        throw new \BadMethodCallException("__set method not allowed");
+    }
+
+    /**
+     * 魔术方法，检查属性是否存在.
+     */
+    public function __isset(string $name): bool
+    {
+        return isset($this->getAttributes()[$name]);
+    }
+
     abstract public function assignModel();
 
     /**
      * 把数据设置为类属性.
      */
-    public static function setAttributes(array $data)
+    public static function setAttributes(array $data): void
     {
         Context::set('attributes', $data);
     }
